a53e3d3583b49483382bc1719ec1da0b062d9910
ThomasGaron <2285014@cmontmorency.qc.ca>
2025-04-16 19:50:51 -0400

    FAIT: LCD fixed

--- view/view.py
+++ view/view.py
@@ -2,10 +2,10 @@ from LCD1602 import CharLCD1602
 
 class view:
     def __init__(self):
-        
-        CharLCD1602.init()
+        self.ecran_lcd = CharLCD1602()
+        self.ecran_lcd.init_lcd(0x27, 1)
         
     def afficher(self, mesure):
-        CharLCD1602.clear()
-        CharLCD1602.write(0, 0, f"Dst: {mesure.distance} cm")
-        CharLCD1602.write(1,0, f"ADC: {mesure.adc}")
+        self.ecran_lcd.clear()
+        self.ecran_lcd.write(0, 0, f"Dst: {mesure.distance} cm")
+        self.ecran_lcd.write(1,0, f"ADC: {mesure.adc}")
