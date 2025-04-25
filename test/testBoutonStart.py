41d1f9e5415ffdc18bdbe03168698f9aeff986b2
ThomasGaron <2285014@cmontmorency.qc.ca>
2025-04-24 22:43:32 -0400

    FAIT : ajout de commentaires pour les tests

--- test/testBoutonStart.py
+++ test/testBoutonStart.py
@@ -9,11 +9,17 @@ Device.pin_factory = MockFactory()
 class TestPlatine(unittest.TestCase):
     def setUp(self):
         self.platine = Platine()
+    
+    # S'assurer qe le bouton est la
+    def test_bouton_initialise(self):
+        self.assertEqual(self.platine.bouton_Start.pin.number, 6)
 
+    # S'assurer du bon fonctionnement lorsqu'il est pressé
     def test_bouton_presse(self):
         self.platine.bouton_Start.pin.drive_low()
         self.assertTrue(self.platine.bouton_Start.is_active)
 
+    # Même chose mais lorsqu'il est relaché
     def test_bouton_relache(self):
         self.platine.bouton_Start.pin.drive_high()
         self.assertFalse(self.platine.bouton_Start.is_active)
